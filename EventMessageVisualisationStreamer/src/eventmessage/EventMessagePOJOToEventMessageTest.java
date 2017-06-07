4c13d2bf72f0db568baad9c311f175923a953927
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-06-07 14:00:31 +0100

    Added expected exception to test

--- EventMessageVisualisationStreamer/src/eventmessage/EventMessagePOJOToEventMessageTest.java
+++ EventMessageVisualisationStreamer/src/eventmessage/EventMessagePOJOToEventMessageTest.java
@@ -24,6 +24,7 @@ public class EventMessagePOJOToEventMessageTest {
 
     @org.junit.Test
     public void runtimeExceptionWhenMessageIdNegative() {
+        exception.expect(RuntimeException.class);
         eventMessagePOJO.setMessageId(-1);
         EventMessagePOJOToEventMessage.convert(eventMessagePOJO);
     }
