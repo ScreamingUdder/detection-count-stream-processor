9d7342e87512b03ffeebe02d6839e6caaf356bb0
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-06-07 15:38:35 +0100

    convertPOJOWithNoDetectorsReturnsByteArray

--- EventMessageVisualisationStreamer/src/eventmessage/EventMessagePOJOToEventMessageTest.java
+++ EventMessageVisualisationStreamer/src/eventmessage/EventMessagePOJOToEventMessageTest.java
@@ -36,4 +36,10 @@ public class EventMessagePOJOToEventMessageTest {
         EventMessagePOJOToEventMessage.convert(eventMessagePOJO);
     }
 
+    @org.junit.Test
+    public void convertPOJOWithNoDetectorsReturnsByteArray() {
+        byte[] result = EventMessagePOJOToEventMessage.convert(eventMessagePOJO);
+        assert result != null;
+    }
+
 }
