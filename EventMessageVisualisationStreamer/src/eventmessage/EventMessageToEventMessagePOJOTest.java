761100c18a96d1b0d0b7612503e63a2b251d6a0e
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-06-12 11:33:56 +0100

    Added test file

---
+++ EventMessageVisualisationStreamer/src/eventmessage/EventMessageToEventMessagePOJOTest.java
@@ -0,0 +1,15 @@
+package eventmessage;
+
+import org.junit.Before;
+
+/**
+ * JUnit tests for EventMessageToEventMessagePOJO utility class.
+ * Created by ISIS, STFC on 12/06/2017.
+ */
+@SuppressWarnings("checkstyle:javadocmethod")
+public class EventMessageToEventMessagePOJOTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+}
