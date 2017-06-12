ce17c945350533f0719597452969c906315b655f
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-06-12 13:46:26 +0100

    Added constants and event message setup before

--- EventMessageVisualisationStreamer/src/eventmessage/EventMessageToEventMessagePOJOTest.java
+++ EventMessageVisualisationStreamer/src/eventmessage/EventMessageToEventMessagePOJOTest.java
@@ -1,5 +1,6 @@
 package eventmessage;
 
+import com.google.flatbuffers.FlatBufferBuilder;
 import org.junit.Before;
 
 /**
@@ -8,8 +9,26 @@ import org.junit.Before;
  */
 @SuppressWarnings("checkstyle:javadocmethod")
 public class EventMessageToEventMessagePOJOTest {
+    private static final long DEFAULT_MESSAGE_ID = 1;
+    private static final long DEFAULT_PULSE_TIME = 1;
+    private static final int[] DEFAULT_DETECTORS = new int[10];
+    private byte[] eventMessageBytes;
     @Before
     public void setUp() throws Exception {
+        //fill detectors array
+        for (int i = 0; i < DEFAULT_DETECTORS.length; i++) {
+            DEFAULT_DETECTORS[i] = i;
+        }
+        //create flatbuffer
+        FlatBufferBuilder builder = new FlatBufferBuilder();
+        EventMessage.startEventMessage(builder);
+        //add parameters
+        EventMessage.addMessageId(builder, DEFAULT_MESSAGE_ID);
+        EventMessage.addPulseTime(builder, DEFAULT_PULSE_TIME);
+        EventMessage.endEventMessage(builder);
+        //add detector ids
+        EventMessage.createDetectorIdVector(builder, DEFAULT_DETECTORS);
+        eventMessageBytes = builder.sizedByteArray();
     }
 
 }
