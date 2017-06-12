aef94450cc2591ef40f0a7f853bf4a1ba72606e9
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-06-12 11:26:11 +0100

    Replaced detector adding with detector vector creation

--- EventMessageVisualisationStreamer/src/eventmessage/EventMessagePOJOToEventMessage.java
+++ EventMessageVisualisationStreamer/src/eventmessage/EventMessagePOJOToEventMessage.java
@@ -24,6 +24,7 @@ public final class EventMessagePOJOToEventMessage {
      */
     public static byte[] convert(final EventMessagePOJO eventMessagePOJO) throws RuntimeException {
 
+        //check for invalid inputs
         if (eventMessagePOJO.getMessageId() < 0) {
             throw new RuntimeException("MessageID cannot be lower than 0.");
         } else if (eventMessagePOJO.getPulseTime() < 0) {
@@ -31,17 +32,21 @@ public final class EventMessagePOJOToEventMessage {
         } else {
             FlatBufferBuilder builder = new FlatBufferBuilder();
             EventMessage.startEventMessage(builder);
-
+            //set params
             EventMessage.addMessageId(builder, eventMessagePOJO.getMessageId());
             EventMessage.addPulseTime(builder, eventMessagePOJO.getPulseTime());
-
-            ArrayList<Integer> detectors = eventMessagePOJO.getDetectors();
-            for (int detectorId : detectors) {
-                EventMessage.addDetectorId(builder, detectorId);
-            }
-
+            //serialise main eventmessage
             int eventMessage = EventMessage.endEventMessage(builder);
             builder.finish(eventMessage);
+            //convert detectors arraylist to primitive arrsy
+            ArrayList<Integer> detectors = eventMessagePOJO.getDetectors();
+            int[] detectorsArray = new int[detectors.size()];
+            for (int i = 0; i < detectors.size(); i++) {
+                detectorsArray[i] = detectors.get(i);
+            }
+            //add detectors array to event message
+            EventMessage.createDetectorIdVector(builder, detectorsArray);
+            //convert to byte array and return
             return builder.sizedByteArray();
         }
     }
