4b83f3f6d1c453c80783b125f4c8e02dd1ac6aa1
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-06-16 16:26:57 +0100

    Deleted broken project

--- EventMessageVisualisationStreamer/src/eventmessage/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ISIS,STFC on 01/06/2017.
- * Package containing the FlatBuffer generated EventMessage and its corresponding POJO.
- * <p>
- * Used for transmitting the data from Detection Events across Kafka.
- * </p>
- */
-package eventmessage;
