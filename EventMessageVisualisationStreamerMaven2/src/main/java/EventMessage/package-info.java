2437ab10754c373fe1543c5cf1903f87566fc244
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-06-16 16:25:10 +0100

    added maven project

---
+++ EventMessageVisualisationStreamerMaven2/src/main/java/EventMessage/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ISIS,STFC on 01/06/2017.
+ * Package containing the FlatBuffer generated EventMessage and its corresponding POJO.
+ * <p>
+ * Used for transmitting the data from Detection Events across Kafka.
+ * </p>
+ */
+package EventMessage;
