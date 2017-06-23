be9dfb7277f57d20dd156eebd1681c7f05e5ac93
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-06-23 10:25:35 +0100

    getMessageIDCorrectWhenConvertingDefaultPOJO

--- EventMessageVisualisationStreamer/src/test/java/EventMessage/EventMessagePOJOToEventMessageTest.java
+++ EventMessageVisualisationStreamer/src/test/java/EventMessage/EventMessagePOJOToEventMessageTest.java
@@ -1,7 +1,65 @@
 package EventMessage;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 /**
- * Created by sci28761 on 23/06/2017.
+ * Created by STFC,ISIS on 07/06/2017.
+ * Unit tests for EventMessagePOJO to EventMessage converter.
  */
+@SuppressWarnings("checkstyle:javadocmethod")
 public class EventMessagePOJOToEventMessageTest {
+    private static final int DEFAULT_MESSAGE_ID = 0;
+    private static final int DEFAULT_PULSE_TIME = 0;
+    private EventMessagePOJO eventMessagePOJO;
+
+    @Before
+    public void setUp() throws Exception {
+        eventMessagePOJO = new EventMessagePOJO(DEFAULT_MESSAGE_ID, DEFAULT_PULSE_TIME);
+    }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @org.junit.Test
+    public void runtimeExceptionWhenMessageIdNegative() {
+        exception.expect(RuntimeException.class);
+        eventMessagePOJO.setMessageId(-1);
+        EventMessagePOJOToEventMessage.convert(eventMessagePOJO);
+    }
+
+    @org.junit.Test
+    public void runtimeExceptionWhenPulseTimeNegative() {
+        exception.expect(RuntimeException.class);
+        eventMessagePOJO.setPulseTime(-1);
+        EventMessagePOJOToEventMessage.convert(eventMessagePOJO);
+    }
+
+    @org.junit.Test
+    public void convertPOJOWithNoDetectorsReturnsByteArray() {
+        byte[] result = EventMessagePOJOToEventMessage.convert(eventMessagePOJO);
+        assertNotNull("Should not be null", result);
+    }
+
+    @org.junit.Test
+    public void convertPOJOWithDetectorsReturnsByteArray() {
+        eventMessagePOJO.addDetector(1);
+        eventMessagePOJO.addDetector(2);
+        eventMessagePOJO.addDetector(3);
+        byte[] result = EventMessagePOJOToEventMessage.convert(eventMessagePOJO);
+        assertNotNull("Should not be null", result);
+    }
+
+    @org.junit.Test
+    public void getMessageIDCorrectWhenConvertingDefaultPOJO() {
+        byte[] eventMessage = EventMessagePOJOToEventMessage.convert(eventMessagePOJO);
+        EventMessagePOJO result = EventMessageToEventMessagePOJO.convert(eventMessage);
+
+        assertEquals(DEFAULT_MESSAGE_ID, result.getMessageId());
+    }
+
 }
