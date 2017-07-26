65b57d8c7b0bae723c2ac797a8fdc7f8b91bcdf0
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-07-26 14:22:22 +0100

    Corrected converter to correctly add detectors

--- EventMessageVisualisationStreamer/src/main/java/EventMessage/EventMessageToEventMessagePOJO.java
+++ EventMessageVisualisationStreamer/src/main/java/EventMessage/EventMessageToEventMessagePOJO.java
@@ -5,7 +5,7 @@ package EventMessage;
  * Created by ISIS, STFC on 12/06/2017.
  */
 public class EventMessageToEventMessagePOJO {
-    public static EventMessagePOJO convert(byte[] eventMessageBytes) {
+    public static EventMessagePOJO convert(final byte[] eventMessageBytes) {
         EventMessage eventMessage = EventMessage.getRootAsEventMessage(java.nio.ByteBuffer.wrap(eventMessageBytes));
         int eventMessageId = (int) eventMessage.messageId();
         EventMessagePOJO eventMessagePOJO = new EventMessagePOJO(eventMessageId, eventMessage.pulseTime());
