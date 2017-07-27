742fb8266ce37facc5d8459db856479e30d5e6e6
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-07-27 08:54:55 +0100

    Added heatmap test file with setup function

---
+++ EventMessageVisualisationStreamer/src/test/java/EventMessage/HeatmapTest.java
@@ -0,0 +1,13 @@
+package EventMessage;
+
+import org.junit.Before;
+
+/**
+ * Created by sci28761 on 27/07/2017.
+ */
+public class HeatmapTest {
+    @Before
+    public void setUp() {
+        Heatmap heatmap = new Heatmap();
+    }
+}
