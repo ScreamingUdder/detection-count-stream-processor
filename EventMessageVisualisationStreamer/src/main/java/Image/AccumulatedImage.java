4fa6c5f4229fb2ed478be328ff14034e7c1d2ab4
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-07-27 11:28:26 +0100

    added positive assertion of new freq to setfreg function along with constant message

--- EventMessageVisualisationStreamer/src/main/java/Image/AccumulatedImage.java
+++ EventMessageVisualisationStreamer/src/main/java/Image/AccumulatedImage.java
@@ -12,6 +12,7 @@ public class AccumulatedImage implements ImageInterface {
     private final String DETECTOR_WITHIN_BOUNDS_ASSERTION_MESSAGE = "Detector index must be within image bounds.";
     private final String IMAGE_SIZE_ABOVE_ZERO_ASSERTION_MESSAGE = "Image size must be above zero.";
     private final String PULSE_TIME_POSITIVE_ASSERTION_MESSAGE = "PulseTime cannot be negative.";
+    private final String FREQUENCY_POSITIVE_ASSERTION_MESSAGE = "The new frequency must be positive.";
 
     public AccumulatedImage(int imageSize) {
         assert imageSize > 0: IMAGE_SIZE_ABOVE_ZERO_ASSERTION_MESSAGE;
@@ -48,6 +49,7 @@ public class AccumulatedImage implements ImageInterface {
     public void setFrequency(int detector, int newFreq) {
         assert detector >= 0: DETECTOR_POSITIVE_ASSERTION_MESSAGE;
         assert detector < getImageSize(): DETECTOR_WITHIN_BOUNDS_ASSERTION_MESSAGE;
+        assert newFreq >= 0: FREQUENCY_POSITIVE_ASSERTION_MESSAGE;
         image[detector] = newFreq;
     }
 }
