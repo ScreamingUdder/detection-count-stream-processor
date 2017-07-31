9b30ba338083dd1a49f469ee9f8bf2722c1810f1
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-07-31 14:41:49 +0100

    added treemap related messages and removed unused ones

--- EventMessageVisualisationStreamer/src/main/java/Image/ImageExceptionMessages.java
+++ EventMessageVisualisationStreamer/src/main/java/Image/ImageExceptionMessages.java
@@ -5,9 +5,8 @@ package Image;
  * Created by ISIS, STFC on 27/07/2017.
  */
 class ImageExceptionMessages {
-    static final String DETECTOR_WITHIN_BOUNDS_ERROR_MESSAGE = "Detector index must be within image bounds.";
-    static final String IMAGE_SIZE_ABOVE_ZERO_ERROR_MESSAGE = "Image size must be above zero.";
+    static final String MISSING_KEY_ERROR_MESSAGE = "The entered detector is not present in the tree map.";
+    static final String DETECTOR_ID_POSITIVE_ERROR_MESSAGE = "Detector IDs cannot be negative.";
     static final String PULSE_TIME_POSITIVE_ERROR_MESSAGE = "PulseTime cannot be negative.";
     static final String FREQUENCY_POSITIVE_ERROR_MESSAGE = "The new frequency must be positive.";
-    static final String IMAGE_SIZE_MISMATCH_ERROR_MESSAGE = "The two images must be of the same size.";
 }
