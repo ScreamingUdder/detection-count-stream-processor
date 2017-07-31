9b43ea6e092fc12559a197aff987191c5c1f9748
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-07-31 13:17:28 +0100

    converted return variable of get image to treemap

--- EventMessageVisualisationStreamer/src/main/java/Image/ImageInterface.java
+++ EventMessageVisualisationStreamer/src/main/java/Image/ImageInterface.java
@@ -1,5 +1,7 @@
 package Image;
 
+import java.util.TreeMap;
+
 /**
  * Image Java interface for storing image data in a static context
  * Created by ISIS, STFC on 27/07/2017.
@@ -11,7 +13,7 @@ public interface ImageInterface {
 
     int getImageSize();
 
-    int[] getImage();
+    TreeMap getImage();
     int getFrequency(int detector);
     void setFrequency(int detector, int newFrequency);
     void incrementFrequency(int detector);
