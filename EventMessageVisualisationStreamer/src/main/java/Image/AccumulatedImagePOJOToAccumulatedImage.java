493f3e346f1185ebafbe267ee54d299d032a0ef5
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-08-01 15:01:17 +0100

    added class

---
+++ EventMessageVisualisationStreamer/src/main/java/Image/AccumulatedImagePOJOToAccumulatedImage.java
@@ -0,0 +1,9 @@
+package Image;
+
+/**
+ * Converts AccumulatedImagePOJO to AccummulatedImage
+ * Created by ISIS, STFC on 01/08/2017.
+ */
+public class AccumulatedImagePOJOToAccumulatedImage {
+
+}
