df0fda20e0fe75b83498022f951cc17c378a2b7a
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-08-23 14:44:57 +0100

    Renamed frame image tests

--- EventMessageVisualisationStreamer/src/test/java/Image/PulseImagePOJOTest.java
+++ EventMessageVisualisationStreamer/src/test/java/Image/PulseImagePOJOTest.java
@@ -7,71 +7,71 @@ import org.junit.Test;
 import java.security.InvalidParameterException;
 
 /**
- * JUnit tests for FrameImagePOJO Java Object
+ * JUnit tests for PulseImagePOJO Java Object
  * Created by ISIS, STFC on 27/07/2017.
  */
-public class FrameImagePOJOTest {
+public class PulseImagePOJOTest {
     private final long DEFAULT_PULSE_TIME = 0L;
-    private FrameImagePOJO frameImagePOJO;
+    private PulseImagePOJO pulseImagePOJO;
     @Before
     public void setUp() {
-        frameImagePOJO = new FrameImagePOJO(DEFAULT_PULSE_TIME);
+        pulseImagePOJO = new PulseImagePOJO(DEFAULT_PULSE_TIME);
     }
 
     // Pulse time tests.
     @Test
     public void getPulseTimeReturnsCorrectOnDefaultHeatmap() {
-        Assert.assertEquals(DEFAULT_PULSE_TIME, frameImagePOJO.getPulseTime());
+        Assert.assertEquals(DEFAULT_PULSE_TIME, pulseImagePOJO.getPulseTime());
     }
 
     @Test
     public void setPulseTimeToPositiveLongWorksCorrectly() {
         long newPulseTime = 1L;
-        frameImagePOJO.setPulseTime(newPulseTime);
+        pulseImagePOJO.setPulseTime(newPulseTime);
 
-        Assert.assertEquals(newPulseTime, frameImagePOJO.getPulseTime());
+        Assert.assertEquals(newPulseTime, pulseImagePOJO.getPulseTime());
     }
 
     @Test(expected = InvalidParameterException.class)
     public void setPulseTimeToNegativeLongThrowsInvalidParameterException() {
         long newPulseTime = -1L;
-        frameImagePOJO.setPulseTime(newPulseTime);
+        pulseImagePOJO.setPulseTime(newPulseTime);
     }
 
     @Test(expected = InvalidParameterException.class)
     public void createNewImageWithNegativePulseTimeThrowsInvalidParameterException() {
         long newPulseTime = -1L;
-        frameImagePOJO = new FrameImagePOJO(newPulseTime);
+        pulseImagePOJO = new PulseImagePOJO(newPulseTime);
     }
 
     // Image size tests.
 
     @Test
     public void getImageSizeCorrectOnDefaultImage() {
-        Assert.assertEquals(0, frameImagePOJO.getImageSize());
+        Assert.assertEquals(0, pulseImagePOJO.getImageSize());
     }
 
     // Get image tests
 
     @Test
     public void getImageReturnsEmptyArrayOfCorrectSizeOnDefaultImage() {
-        Assert.assertEquals(0, frameImagePOJO.getImage().size());
-        Assert.assertEquals(0, frameImagePOJO.getImage().keySet().size());
+        Assert.assertEquals(0, pulseImagePOJO.getImage().size());
+        Assert.assertEquals(0, pulseImagePOJO.getImage().keySet().size());
     }
 
     // Get frequency tests.
 
     @Test(expected = InvalidParameterException.class)
     public void getFrequencyOfNonSetDetectorReturnsThrowsInvalidParameter() {
-        frameImagePOJO.getFrequency(0);
+        pulseImagePOJO.getFrequency(0);
     }
 
     @Test
     public void getFrequencyOfSetDetectorReturnsCorrect() {
         int detector = 1;
         int newFreq = 5;
-        frameImagePOJO.setFrequency(detector, newFreq);
-        Assert.assertEquals(newFreq, frameImagePOJO.getFrequency(detector));
+        pulseImagePOJO.setFrequency(detector, newFreq);
+        Assert.assertEquals(newFreq, pulseImagePOJO.getFrequency(detector));
 
     }
 
@@ -79,51 +79,51 @@ public class FrameImagePOJOTest {
 
     @Test(expected = InvalidParameterException.class)
     public void setFrequencyOfNegativeDetectorThrowsInvalidParameterException() {
-        frameImagePOJO.setFrequency(-1, 5);
+        pulseImagePOJO.setFrequency(-1, 5);
     }
 
     @Test(expected = InvalidParameterException.class)
     public void setFrequencyOfDetectorToNegativeThrowsInvalidParameterException() {
-        frameImagePOJO.setFrequency(0, -1);
+        pulseImagePOJO.setFrequency(0, -1);
     }
 
     @Test
     public void getFrequencyOfDetectorAfterSettingFrequencyOfNewDetectorReturnsCorrect() {
         int detector = 0;
         int newFreq = 1;
-        frameImagePOJO.setFrequency(0, 1);
-        Assert.assertEquals(newFreq, frameImagePOJO.getFrequency(detector));
+        pulseImagePOJO.setFrequency(0, 1);
+        Assert.assertEquals(newFreq, pulseImagePOJO.getFrequency(detector));
     }
 
     @Test public void getFrequencyOfDetectorAfterUpdatingExistingDetectorReturnsCorrect() {
         int detector = 0;
         int newFreq = 5;
-        frameImagePOJO.setFrequency(detector, 0);
-        frameImagePOJO.setFrequency(detector, newFreq);
+        pulseImagePOJO.setFrequency(detector, 0);
+        pulseImagePOJO.setFrequency(detector, newFreq);
 
-        Assert.assertEquals(newFreq, frameImagePOJO.getFrequency(detector));
+        Assert.assertEquals(newFreq, pulseImagePOJO.getFrequency(detector));
     }
 
     // Increment frequency tests.
 
     @Test
     public void getFrequencyAfterIncrementFrequencyOfNewDetectorReturnsCorrect() {
-        frameImagePOJO.incrementFrequency(0);
-        Assert.assertEquals(1, frameImagePOJO.getFrequency(0));
+        pulseImagePOJO.incrementFrequency(0);
+        Assert.assertEquals(1, pulseImagePOJO.getFrequency(0));
     }
 
     @Test
     public void getFrequencyAfterIncrementOfExistingDetectorReturnsCorrect() {
         int detector = 0;
         int origFreq = 1;
-        frameImagePOJO.setFrequency(detector, origFreq);
-        frameImagePOJO.incrementFrequency(detector);
-        Assert.assertEquals(origFreq + 1, frameImagePOJO.getFrequency(detector));
+        pulseImagePOJO.setFrequency(detector, origFreq);
+        pulseImagePOJO.incrementFrequency(detector);
+        Assert.assertEquals(origFreq + 1, pulseImagePOJO.getFrequency(detector));
     }
 
     @Test(expected = InvalidParameterException.class)
     public void incrementFrequencyOfNegativeDetectorThrowsInvalidParameterException() {
-        frameImagePOJO.incrementFrequency(-1);
+        pulseImagePOJO.incrementFrequency(-1);
     }
 
 }
