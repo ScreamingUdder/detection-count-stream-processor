e7052ef5791574627bc9cc13b823c18623921761
DChambersSTFC <dylan.chambers@stfc.ac.uk>
2017-08-25 14:29:07 +0100

    Renamed files and added package info

---
+++ EventMessageVisualisationStreamer/src/main/java/ExampleStreamConsumers/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by ISIS,STFC on 20/08/2017.
+ * Package containing example Kafka Consumers
+ * for consuming from the default topics that the EventMessage and PulseImage processors
+ */
+package ExampleStreamConsumers;
